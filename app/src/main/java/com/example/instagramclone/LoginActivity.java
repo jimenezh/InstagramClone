aaa042a4bb4eae3be1a1c199efa55fd968b6fdcf
jimenezh <jimenezshr@fb.com>
2020-07-06 13:35:56 -0700

    Sign in

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -2,13 +2,57 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.instagramclone.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = binding.etUsername.getText().toString();
+                String password = binding.etPassword.getText().toString();
+                logInUser(username, password);
+            }
+        });
+    }
+
+    private void logInUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null){
+                    Log.i(TAG,user.getUsername()+" is signed in");
+                    Toast.makeText(LoginActivity.this, user.getUsername()+" is signed in",Toast.LENGTH_SHORT);
+                    launchMainActivity();
+                } else{
+                    Log.e(TAG,"Parse Log In exception", e);
+                    Toast.makeText(LoginActivity.this,"Could not sign in", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
