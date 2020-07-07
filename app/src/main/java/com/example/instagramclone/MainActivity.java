c9fcf27de587819d786239d34c2e438f969ed903
jimenezh <jimenezshr@fb.com>
2020-07-07 11:05:13 -0700

    Passed camera logic to ComposeFragment

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 import com.example.instagramclone.databinding.ActivityLoginBinding;
 import com.example.instagramclone.databinding.ActivityMainBinding;
 import com.example.instagramclone.models.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -33,9 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     public static final String TAG = "MainActivity";
-    private File photoFile;
-    public String photoFileName = "photo.jpg";
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
 
 
     @Override
@@ -44,116 +44,29 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        queryPosts();
-        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String description = binding.etDescription.getText().toString();
-                if(description.isEmpty())
-                    Toast.makeText(MainActivity.this,"Description cannot be empty", Toast.LENGTH_SHORT).show();
-                else if(photoFile == null || binding.ivPostImage.getDrawable() == null)
-                    Toast.makeText(MainActivity.this, "No image!", Toast.LENGTH_SHORT).show();
-                else
-                    savePost(description, ParseUser.getCurrentUser(), photoFile);
-            }
-        });
-
-        binding.btnPic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchCamera();
-            }
-        });
+        setBottomNavigationListener();
 
     }
 
-    private void savePost(String description, ParseUser currentUser, File photoFile) {
-        Post p = new Post();
-        p.setDescription(description);
-//        p.setImage();
-        p.setUser(currentUser);
-        p.setImage(new ParseFile(photoFile));
-        p.saveInBackground(new SaveCallback() {
+    private void setBottomNavigationListener() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void done(ParseException e) {
-                if(e != null) {
-                    Log.e(TAG, "Error saving post", e);
-                    return;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        // do something here
+                        break;
+                    case R.id.action_camera:
+                        // do something here
+                        break;
+                    case R.id.action_profile:
+                        // do something here
+                        break;
+                    default: break;
                 }
-                Log.i(TAG, "Posted!");
-                Toast.makeText(MainActivity.this, "Posted",Toast.LENGTH_SHORT).show();
-                binding.etDescription.setText("");
+
+                return true;
             }
         });
-    }
-
-    private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if(e != null){
-                    Log.e(TAG, "Error in query", e);
-                    return;
-                }
-                for(Post p : posts){
-                    Log.i(TAG,p.getDescription());
-                }
-            }
-        });
-    }
-
-    public void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                binding.ivPostImage.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
