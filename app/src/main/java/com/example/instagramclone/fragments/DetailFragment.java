6f7ba93a48655452042d106df3540528509136df
jimenezh <jimenezshr@fb.com>
2020-07-08 10:25:00 -0700

    Initial fragment template

---
+++ app/src/main/java/com/example/instagramclone/fragments/DetailFragment.java
@@ -0,0 +1,29 @@
+package com.example.instagramclone.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instagramclone.R;
+import com.example.instagramclone.databinding.FragmentDetailBinding;
+
+public class DetailFragment extends Fragment {
+
+    FragmentDetailBinding binding;
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentDetailBinding.inflate(getLayoutInflater());
+        // Inflate the layout for this fragment
+        return binding.getRoot();
+    }
+}
