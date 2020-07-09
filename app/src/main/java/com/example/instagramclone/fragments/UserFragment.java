2651d4e346f850be2fb292b116a515bd186c04e1
jimenezh <jimenezshr@fb.com>
2020-07-09 09:57:48 -0700

    Basic template for user fragment

---
+++ app/src/main/java/com/example/instagramclone/fragments/UserFragment.java
@@ -0,0 +1,35 @@
+package com.example.instagramclone.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instagramclone.R;
+import com.example.instagramclone.databinding.FragmentUserBinding;
+
+
+public class UserFragment extends Fragment {
+
+    FragmentUserBinding binding;
+
+    public UserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentUserBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+}
