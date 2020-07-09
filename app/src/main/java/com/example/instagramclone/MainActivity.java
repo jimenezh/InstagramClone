6255c740b6eb99a01d68eb219976aa896a63bb0c
jimenezh <jimenezshr@fb.com>
2020-07-09 10:38:14 -0700

    Renamed PostAdapter to FeedAdapter

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,56 +1,32 @@
 package com.example.instagramclone;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcel;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.example.instagramclone.adapters.PostsAdapter;
-import com.example.instagramclone.databinding.ActivityLoginBinding;
+import com.example.instagramclone.adapters.FeedAdapter;
 import com.example.instagramclone.databinding.ActivityMainBinding;
 import com.example.instagramclone.fragments.ComposeFragment;
-import com.example.instagramclone.fragments.DetailFragment;
-import com.example.instagramclone.fragments.PostFragment;
+import com.example.instagramclone.fragments.FeedFragment;
 import com.example.instagramclone.fragments.ProfileFragment;
 import com.example.instagramclone.models.Post;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements PostsAdapter.PostAdapterListener {
+public class MainActivity extends AppCompatActivity implements FeedAdapter.PostAdapterListener {
 
     ActivityMainBinding binding;
     public static final String TAG = "MainActivity";
     final FragmentManager fragmentManager = getSupportFragmentManager();
     // Fragments for tab navigation
-    final PostFragment postFragment = new PostFragment();
+    final FeedFragment feedFragment = new FeedFragment();
     final ComposeFragment composeFragment = new ComposeFragment();
     final ProfileFragment profileFragment = new ProfileFragment();
 
@@ -71,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements PostsAdapter.Post
                 Fragment fragment = null;
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        fragment = postFragment;
+                        fragment = feedFragment;
                         break;
                     case R.id.action_camera:
                         fragment = composeFragment;
